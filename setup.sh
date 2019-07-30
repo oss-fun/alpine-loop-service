ed8a64a9f3d025d0e62a6fa3346a7b15db5bcce6
yochan <youtanagai@gmail.com>
2019-07-30 22:21:17 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/ash
-cp ./bin/loop /usr/bin/loop
-cp ./service/loop /etc/init.d/loop
-rc-service loop start
-rc-update add loop
-rc-status
-
+cp ./bin/loop /usr/bin/loop #バイナリの配置
+cp ./service/loop /etc/init.d/loop #systemdで言うところのunitファイルを配置
+rc-service loop start #サービスの起動
+rc-update add loop #サービスの自動起動登録 systemdで言うところのenable
+rc-status #状態確認
